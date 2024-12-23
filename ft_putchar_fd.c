bfa4d9344710e0d3827c1fcff31ba8db79953040
mohamed <khaldimohamed82@gmail.com>
2024-12-23 19:50:40 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 01:21:53 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/12/14 00:43:51 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/12/23 19:50:38 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 int	ft_putchar_fd(char c, int fd)
 {
-	write(fd, &c, 1);
-	return (1);
+	return (write(fd, &c, 1));
 }
